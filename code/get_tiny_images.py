0c7ec159325bd6f14b08f052d3e406f99c25deca
Shreyanshu Bhushan <33536680+shreyanshu09@users.noreply.github.com>
2020-09-28 10:03:24 +0530

    Add files via upload

---
+++ code/get_tiny_images.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+import pdb
+import numpy as np
+
+def get_tiny_images(image_paths):
+
+    N = len(image_paths)
+    size = 16
+    
+    tiny_images = []
+    
+    for each in image_paths:
+        image = Image.open(each)
+        image = image.resize((size, size))
+        image = (image - np.mean(image))/np.std(image)
+        image = image.flatten()
+        tiny_images.append(image)
+        
+    tiny_images = np.asarray(tiny_images)
+
+    return tiny_images
